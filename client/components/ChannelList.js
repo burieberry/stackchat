049c3bd7368cb94d5dc837d3218391e17bea7ccc
Burcu Noyan <burcunoyan@gmail.com>
2017-09-20 23:26:59 -0400

    render message counts

--- client/components/ChannelList.js
+++ client/components/ChannelList.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import store, { gotMessagesFromServer } from '../store';
 
-// These values are all hardcoded...for now!
-// Soon, we'll fetch them from the server!
 const RANDOM_CHANNEL = '/channels/1';
 const GENERAL_CHANNEL = '/channels/2';
 const DOGS_CHANNEL = '/channels/3';
@@ -10,31 +9,52 @@ const LUNCH_CHANNEL = '/channels/4';
 
 export default class ChannelList extends Component {
 
+  constructor() {
+    super();
+    this.state = store.getState();
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => {
+      this.setState(store.getState());
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
   render () {
+    const { messages } = this.state;
+    const channelIds = [ 1, 2, 3, 4];
+    var messageList = channelIds.map(id => {
+      return messages.filter(message => message.channelId === id );
+    });
+
     return (
       <ul>
         <li>
           <NavLink to={RANDOM_CHANNEL} activeClassName="active">
             <span># really_random</span>
-            <span className="badge">0</span>
+            <span className="badge">{ messageList[0].length }</span>
           </NavLink>
         </li>
         <li>
           <NavLink to={GENERAL_CHANNEL} activeClassName="active">
             <span># generally_speaking</span>
-            <span className="badge">0</span>
+            <span className="badge">{ messageList[1].length }</span>
           </NavLink>
         </li>
         <li>
           <NavLink to={DOGS_CHANNEL} activeClassName="active">
             <span># dogs_of_fullstack</span>
-            <span className="badge">0</span>
+            <span className="badge">{ messageList[2].length }</span>
           </NavLink>
         </li>
         <li>
           <NavLink to={LUNCH_CHANNEL} activeClassName="active">
             <span># lunch_planning</span>
-            <span className="badge">0</span>
+            <span className="badge">{ messageList[3].length }</span>
           </NavLink>
         </li>
       </ul>
